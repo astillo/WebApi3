62a189b312f33b02923964d7ba48e794bd0e821c
astillo <clomes.alex@gmail.com>
2019-09-04 12:30:56 -0500

    middleware set up

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
 const server = express()
+// middleware goes here
 
+server.use(express.json())
+
+//routes go here
 server.get('/', (req, res) => {
     res.status(201).json({ api: 'up' })
 })
