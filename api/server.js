621bad0a3ae78b65fae7a8e5a86dbc012636d578
astillo <clomes.alex@gmail.com>
2019-09-04 12:43:31 -0500

    express routers set up

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const productsRouter = require('../products/products-router.js')
 
 const server = express()
 // middleware goes here
 
 server.use(express.json())
+
+server.use('/products', productsRouter)
 
 //routes go here
 server.get('/', (req, res) => {
